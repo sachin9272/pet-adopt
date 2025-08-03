0bd7ac0a6ae728c9f3ddc95f0002487814229c5c
Sachin Singh <sachinsingh9272@gmail.com>
2025-08-03 23:28:09 +0530

    Created home tab

---
+++ Frontend/app/(tabs)/home.jsx
@@ -0,0 +1,46 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
+import PetListByCategory from '../../components/Home/PetListByCategory'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Colors from '../../constants/Colors'
+
+export default function home() {
+  return (
+    <View 
+        style={{padding:20, marginTop: 20}}
+    >
+      {/* Header */}
+      <Header/>
+      {/* Slider */}
+      <Slider/>
+      {/* List of Pets */}
+      <PetListByCategory/>
+      {/* Add New Pet Option  */}
+      <TouchableOpacity style={styles.addNewPetContainer}>
+        <MaterialIcons name="pets" size={24} color={Colors.PRIMARY} />
+        <Text style={{fontFamily:'outfit-medium', color:Colors.PRIMARY, fontSize:18}}>Add New Post</Text>
+
+      </TouchableOpacity>
+    </View>
+  )
+}   
+
+
+const styles = StyleSheet.create({
+    addNewPetContainer: {
+        display:'flex',
+        flexDirection:'row',
+        gap:10,
+        alignItems:'center',
+        padding:20,
+        marginTop: 20,
+        backgroundColor:Colors.SECONDARY,
+        borderWidth: 2,
+        borderColor: Colors.PRIMARY,
+        borderRadius: 15,
+        borderStyle: 'dashed',
+        justifyContent:'center'
+      }
+})
