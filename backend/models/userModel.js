9627ea47512a58d105990f125bfd6de3e2cc3235
Sachin Singh <sachinsingh9272@gmail.com>
2025-08-11 22:28:13 +0530

    Created user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String
+    }
+}, {timestamps:true});
+
+const User = mongoose.model('User', userSchema);
+export default User;
