f329747ca174a422a10b3b85cd8e6e240fc6a8f4
Sachin Singh <sachinsingh9272@gmail.com>
2025-08-16 21:07:32 +0530

    Updated pet list page

--- Frontend/components/Home/PetList.jsx
+++ Frontend/components/Home/PetList.jsx
@@ -9,40 +9,62 @@ export default function PetList({ pet }) {
   return (
     <View
       style={{
-        backgroundColor: Colors.SECONDARY,
-        borderRadius: 14,
-        marginBottom: 20,
-        padding: 12,
-        width: cardWidth,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOpacity: 0.1,
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 4,
-        elevation: 2,
+        padding:10,
+        marginRight:15,
+        backgroundColor: Colors.WHITE,
+        borderRadius: 10,
+
+        // marginBottom: 20,
+        // width: cardWidth,
+        // alignItems: 'center',
+        // shadowColor: '#000',
+        // shadowOpacity: 0.1,
+        // shadowOffset: { width: 0, height: 2 },
+        // shadowRadius: 4,
+        // elevation: 2,
+        // height:123
       }}
     >
       <Image
         source={{ uri: pet.image }}
         style={{
-          width: 80,
-          height: 80,
+          width: 150,
+          height: 135,
+          objectFit:'cover',
           borderRadius: 10,
-          marginBottom: 10,
-          backgroundColor: '#ccc',
+
+          
         }}
         resizeMode="cover"
       />
       <Text
         style={{
           fontFamily: 'outfit-medium',
-          fontSize: 16,
-          marginBottom: 4,
-          textAlign: 'center',
+          fontSize: 18,
+  
         }}
       >
         {pet.name}
       </Text>
+      <View style={{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems: 'center'
+      }}>
+        <Text style={{color:Colors.GRAY,
+          fontFamily:'outfit'
+        }}>
+          {pet?.breed}
+        </Text>
+        <Text style={{fontFamily:'outfit',
+          color: Colors.PRIMARY,
+          paddingHorizontal: 7,
+          borderRadius: 10,
+          fontSize: 11,
+          backgroundColor: Colors.LIGHT_PRIMARY
+        }}>{pet?.age} YRS</Text>
+      </View>
       <Text
         style={{
           fontFamily: 'outfit',
