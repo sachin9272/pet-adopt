c31ac44ab40a46724a0e807ce83452fb025f4d5b
Sachin Singh <sachinsingh9272@gmail.com>
2025-08-16 23:55:43 +0530

    Updated pet details

--- Frontend/components/Home/PetListByCategory.jsx
+++ Frontend/components/Home/PetListByCategory.jsx
@@ -4,12 +4,12 @@ import Category from './Category';
 import PetList from './PetList';
 
 const pets = [
-  { id: '1', name: 'Max', age:2, category: 'Dogs', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV1RlrbxJfEqwRJACDSVzfUmCqSdrP8QUkYA&s', description: 'Friendly Golden Retriever' },
-  { id: '2', name: 'Bella', age:1, category: 'Dogs', image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Dog_Breeds.jpg', description: 'Loyal Labrador' },
-  { id: '3', name: 'Whiskers', age:2, category: 'Cats', image: 'https://cdn-icons-png.flaticon.com/128/616/616430.png', description: 'Playful Tabby' },
-  { id: '4', name: 'Mittens', age:9, category: 'Cats', image: 'https://cdn-icons-png.flaticon.com/128/616/616430.png', description: 'Cuddly Persian' },
-  { id: '5', name: 'Bunny', age:5, category: 'Rabbits', image: 'https://cdn-icons-png.flaticon.com/512/4185/4185015.png', description: 'Fluffy Lop' },
-  { id: '6', name: 'Tweety', age:4, category: 'Birds', image: 'https://cdn-icons-png.flaticon.com/512/2832/2832126.png', description: 'Chirpy Canary' },
+  { id: '1', name: 'Max', age:2, address:'Lucknow, India', category: 'Dogs', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV1RlrbxJfEqwRJACDSVzfUmCqSdrP8QUkYA&s', description: 'Friendly Golden Retriever' },
+  { id: '2', name: 'Bella', age:1, address:'Lucknow, India', category: 'Dogs', image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Dog_Breeds.jpg', description: 'Loyal Labrador' },
+  { id: '3', name: 'Whiskers', age:2, address:'Lucknow, India', category: 'Cats', image: 'https://cdn-icons-png.flaticon.com/128/616/616430.png', description: 'Playful Tabby' },
+  { id: '4', name: 'Mittens', age:9, address:'Lucknow, India', category: 'Cats', image: 'https://cdn-icons-png.flaticon.com/128/616/616430.png', description: 'Cuddly Persian' },
+  { id: '5', name: 'Bunny', age:5, address:'Lucknow, India', category: 'Rabbits', image: 'https://cdn-icons-png.flaticon.com/512/4185/4185015.png', description: 'Fluffy Lop' },
+  { id: '6', name: 'Tweety', age:4, address:'Lucknow, India', category: 'Birds', image: 'https://cdn-icons-png.flaticon.com/512/2832/2832126.png', description: 'Chirpy Canary' },
 ];
 
 export default function PetListByCategory() {
