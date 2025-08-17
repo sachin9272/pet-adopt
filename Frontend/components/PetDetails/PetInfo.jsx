cb970c0e054369cd19bfece075267e1fcf82fccf
Sachin Singh <sachinsingh9272@gmail.com>
2025-08-17 23:16:36 +0530

    Created Pet Info Page

---
+++ Frontend/components/PetDetails/PetInfo.jsx
@@ -0,0 +1,43 @@
+import { View, Image, Text } from "react-native";
+import React from "react";
+import Colors from "../../constants/Colors";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function PetInfo({ pet }) {
+  return (
+    <View>
+      <Image
+        source={{ uri: pet.image }}
+        style={{
+          width: "100%",
+          height: 400,
+          objectFit: "cover",
+        }}
+      />
+      <View style={{ 
+        padding: 20,
+        display: 'flex',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        alignItems:'center'
+       }}>
+        <View>
+          <Text
+            style={{
+              fontFamily: "outfit-bold",
+              fontSize: 27,
+            }}
+          >
+            {pet?.name}
+          </Text>
+          <Text
+            style={{ fontFamily: "outfit", fontSize: 16, color: Colors.GRAY }}
+          >
+            {pet?.address}
+          </Text>
+        </View>
+        <Ionicons name="heart-outline" size={30} color="black" />
+      </View>
+    </View>
+  );
+}
