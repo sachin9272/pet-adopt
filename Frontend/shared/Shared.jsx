ecc22956e744fe0caa2e6f115e9d4fc427f2e8d7
Sachin Singh <sachinsingh9272@gmail.com>
2025-08-18 13:37:02 +0530

    Updaed clerk authentication

---
+++ Frontend/shared/Shared.jsx
@@ -0,0 +1,3 @@
+export const GetFavList=(user)=>{
+    
+}
--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -1,6 +1,7 @@
 import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from '../config/cloudinaryConfig.js'; // Import config
+// import cloudinary from '../config/cloudinaryConfig.js'; // Import config
+import cloudinary from '../utils/cloudinary.js'
 
 const storage = new CloudinaryStorage({
   cloudinary,
