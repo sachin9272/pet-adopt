49ccc3c988cea09bb7d37295b5042293011dcf31
Sachin Singh <sachinsingh9272@gmail.com>
2025-08-31 23:46:26 +0530

    Updated pet controller

--- backend/controllers/petController.js
+++ backend/controllers/petController.js
@@ -1,27 +1,30 @@
 import Pet from "../models/petModel.js";
 
 export const AddPet = async(req, res) => {
-    const userId = req?.user?.userId;
-    const {petName, breed, age, category, gender, weight, address, about} = req.body;
-    try {
-        if(!petName || !breed || !age || !category || !weight || !gender || !address || !about){
+    console.log("API hitted::::====>")
+    // const userId = req?.user?.userId;
+    console.log('Bodyyy-->', req.body);
+    const userId = '68a213abf1abebf8f53bab35';
+    const {name, breed, age, category, gender, address, about} = req.body; // Add weight
+    // try {
+        if(!name || !breed || !age || !category || !gender || !address || !about){
             return res.status(400).json({message:"Please provide all details."});
         }
         const pet = await Pet.create({
-            petName,
+            petName: name,
             breed, 
             age, 
             category, 
             gender, 
-            weight, 
+            // weight, 
             address, 
             about,
             user:userId
         });
-        return res.status(201).json({message:"Pet added successfully", success: true})
-    } catch (error) {
-        return res.status(500).json({message:"Internal Server Error", success: false});
-    }
+        return res.status(201).json({message:"Pet added successfully", pet, success: true})
+    // } catch (error) {
+    //     return res.status(500).json({message:"Internal Server Error", success: false});
+    // }
 }
 
 const DeletePet = async(req, res) => {
