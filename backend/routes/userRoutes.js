280905855b239e6b1668bdcd0953cc633d90c240
Sachin Singh <sachinsingh9272@gmail.com>
2025-08-31 23:47:04 +0530

    Updated user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { generateBackendToken } from "../controllers/userController.js";
+import { generateBackendToken, GetAllFavPet, toggleFavPet } from "../controllers/userController.js";
 // import  upload  from "../middlewares/upload.js";
 const router = Router();
 
 router.post('/auth/exchange', generateBackendToken);
+router.post('/fav/toggle',toggleFavPet);
+router.get('/favPet/:userId', GetAllFavPet);
 
 export default router;
